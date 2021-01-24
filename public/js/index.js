306c29e286dfad7661030603c552a95ceeed5d28
NaRaYeN <36738256+Narayen@users.noreply.github.com>
2021-01-24 23:09:18 +0530

    trail 2

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+var deg = 6;
+var hr = document.querySelector('#hr');
+var min = document.querySelector('#min');
+var sec = document.querySelector('#sec');
+
+setInterval(() => {
+    let today = new Date();
+    let hh = today.getHours() * 30;
+    let mm = today.getMinutes() * deg;
+    let ss = today.getSeconds() * deg;
+    
+    hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+    min.style.transform = `rotateZ(${mm}deg)`;
+    sec.style.transform = `rotateZ(${ss}deg)`;
+})
